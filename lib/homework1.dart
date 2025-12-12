0ac7db1c1590f485eb015273233a30cd2fa170ad
禾火秋 <99186207+EDCL16@users.noreply.github.com>
2025-12-12 15:49:04 +0800

    init project

---
+++ lib/homework1.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40),
+
+            // 標題
+            const Text(
+              "Homework 1",
+              style: TextStyle(fontSize: 26),
+            ),
+
+            const SizedBox(height: 30),
+
+            // 圓型頭像底色 + 圖片
+            CircleAvatar(
+              radius: 180,
+              backgroundColor: Colors.black,
+              child: Image.asset(
+                'Images/mori.jpg', // 放你的圖片
+                height: 150,
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            // 名字
+            const Text(
+              "Mori Calliope",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            // 分隔線
+            Container(
+              width: 300,
+              height: 2,
+              color: Colors.grey,
+              margin: const EdgeInsets.symmetric(vertical: 8),
+            ),
+
+            // 副標
+            const Text(
+              "Gug!",
+              style: TextStyle(fontSize: 29, color: Colors.grey),
+            ),
+
+            const SizedBox(height: 30),
+
+            // Email 按鈕
+            _buildButton(
+              icon: Icons.email,
+              text: "s111216067@stu.ntue.edu.tw",
+              onTap: () {},
+            ),
+
+            const SizedBox(height: 15),
+
+            // Phone 按鈕
+            _buildButton(
+              icon: Icons.phone,
+              text: "0909-919-gigi",
+              onTap: () {},
+            ),
+
+            const SizedBox(height: 15),
+            // Phone 按鈕
+            _buildButton(
+              icon: Icons.videogame_asset,
+              text: "Favorite game : LoL",
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 自訂黃色按鈕
+  Widget _buildButton({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return Container(
+      width: 300,
+      height: 55,
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15),
+          onTap: onTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 26),
+              const SizedBox(width: 12),
+              Text(
+                text,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: const Text('app title')),
+        body: Column(
+          children: [
+            const Center(
+
+            ),
+            Icon(Icons.add_circle),
+            Container(
+              width: 200,
+              height: 50,
+              color: Colors.greenAccent,
+              child: Text(
+              'Hello, Flutter\n999!',
+              style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(width: 100, height: 50),
+            Expanded(child: Container(width: 150, decoration:
+              BoxDecoration(
+                  borderRadius: BorderRadius.circular(20)
+                ,color: Colors.blue,
+              ),
+              child: SizedBox(width: 50, height: 50,
+              child: Image(image:AssetImage('Images/image1.jpg')),
+            ),
+              )
+            ),
+            SizedBox(width: 100, height: 50),
+            Container(width: 200, height: 100, color: Colors.greenAccent,child: Text(
+              'Hello, Flutter\n999!',
+              style: TextStyle(fontSize: 20),
+            ),),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
